b9d21aec18d38b66f4401f5cabf06075a888c5a9
Silly-Bo <614924406@qq.com>
2017-02-20 23:43:01 +0800

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,23 +1,8 @@
 <?php
+// echo 'hello';
+$msg=array('你好','吃了么？','很高兴认识你','你叫什么名字？');
 
-
-	$messages = array(
-		'你说啥？',
-		'你也好',
-		'你找我有啥事？',
-		'我在吃饭！'
-	);
-
-	// 通过array_rand()随机获取数组下标
-	// var_dump(array_rand($messages));
-
-	//方法的含义，在其中传入一个数组，返回值是索引 有时是0 有时是3这样的随机数
-
-	$key = array_rand($messages);　
-
-	echo $messages[$key];
-
-	sleep(1);
-
-
+// 定义一个随机值
+$k=array_rand($msg,1);
+echo $msg[$k];
 ?>
